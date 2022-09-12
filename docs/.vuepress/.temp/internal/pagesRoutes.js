7024f69ec56f42782d33a3cc277cab56e01e3658
徐静波 <897884964@qq.com>
2022-09-12 11:35:20 +0800

    测试

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++
@@ -1,7 +0,0 @@
-export const pagesRoutes = [
-  ["v-8daa1a0e","/",{"title":""},["/index.html","/README.md"]],
-  ["v-3706649a","/404.html",{"title":""},["/404"]],
-  ["v-589fcedc","/test/solidity.html",{"title":"Solidity学习笔记"},["/test/solidity","/test/solidity.md"]],
-  ["v-c62d51ac","/guide/solidity.html",{"title":"Solidity学习笔记"},["/guide/solidity","/guide/solidity.md"]],
-  ["v-fffb8e28","/guide/",{"title":"测试"},["/guide/index.html","/guide/README.md"]],
-]
--- docs/README.md
+++
@@ -1,8 +0,0 @@
-# Hello World
-## 这是VuePress
-测试文字
-## 这是文档搭建工具
-[配置参考](./guide/solidity.md)
-
-## 测试测试
-测试文字
